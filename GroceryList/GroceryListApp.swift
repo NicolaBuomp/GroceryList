4c97b345795efca5e2e2b619b28317aea836f0a8
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-28 10:23:24 +0100

    Initial Commit

---
+++ GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Nicola Buompane on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
